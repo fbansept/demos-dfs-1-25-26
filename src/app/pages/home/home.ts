4aa18547fa24792ceedd0231ab4a4ad90cfe1104
fbansept <bansept.franck@gmail.com>
2026-01-27 09:35:27 +0100

    ajout local storage

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -11,28 +11,57 @@ import { MatButtonModule } from '@angular/material/button';
 export class Home {
   urlImageSaisie = '';
 
-  categories = [
-    {
-      titre: 'A',
-      images: [
-        'https://blog.hellofresh.co.uk/wp-content/uploads/2021/03/HF201125_R209_W02_FR_RFR20161819-1_MB_Main_low-scaled.jpg',
-        'https://kissmychef.com/wp-content/uploads/2020/07/cornetteria.jpg',
-      ],
-    },
-    { titre: 'B', images: [] },
-    {
-      titre: 'C',
-      images: [
-        'https://img-3.journaldesfemmes.fr/BNubGw2ChgpFyw3eK2g-PMwF28Y=/1240x/smart/7231e1a7ad4a4fbb94f3498c11392d23/ccmcms-jdf/36619834.jpg',
-      ],
-    },
-    { titre: 'D', images: [] },
-  ];
+  // categories = [
+  //   {
+  //     titre: 'A',
+  //     images: [
+  //       'https://blog.hellofresh.co.uk/wp-content/uploads/2021/03/HF201125_R209_W02_FR_RFR20161819-1_MB_Main_low-scaled.jpg',
+  //       'https://kissmychef.com/wp-content/uploads/2020/07/cornetteria.jpg',
+  //     ],
+  //   },
+  //   { titre: 'B', images: [] },
+  //   {
+  //     titre: 'C',
+  //     images: [
+  //       'https://img-3.journaldesfemmes.fr/BNubGw2ChgpFyw3eK2g-PMwF28Y=/1240x/smart/7231e1a7ad4a4fbb94f3498c11392d23/ccmcms-jdf/36619834.jpg',
+  //     ],
+  //   },
+  //   { titre: 'D', images: [] },
+  // ];
+
+  // categories: Categorie[] = [
+  //   { titre: 'A', images: [] },
+  //   { titre: 'B', images: [] },
+  //   { titre: 'C', images: [] },
+  //   { titre: 'D', images: [] },
+  // ];
+
+  categories: Categorie[] = [];
+
+  ngOnInit() {
+    const categoriesSauvegardeesJson = localStorage.getItem('sauvegarde');
+
+    if (categoriesSauvegardeesJson) {
+      this.categories = JSON.parse(categoriesSauvegardeesJson);
+    } else {
+      this.categories = [
+        { titre: 'A', images: [] },
+        { titre: 'B', images: [] },
+        { titre: 'C', images: [] },
+        { titre: 'D', images: [] },
+      ];
+    }
+  }
+
+  sauvegarder() {
+    localStorage.setItem('sauvegarde', JSON.stringify(this.categories));
+  }
 
   ajouterImage() {
     if (this.urlImageSaisie !== '') {
       this.categories[0].images.push(this.urlImageSaisie);
       this.urlImageSaisie = '';
+      this.sauvegarder();
     }
   }
 
@@ -45,17 +74,12 @@ export class Home {
 
     //on supprime l'image de la categorie actuelle
     this.categories[indexCategorie].images.splice(indexImage, 1);
+    this.sauvegarder();
   }
 
   supprimerImage(indexCategorie: number, indexImage: number) {
     //on supprime l'image de la categorie actuelle
     this.categories[indexCategorie].images.splice(indexImage, 1);
+    this.sauvegarder();
   }
 }
-
-//ajouter un bouton pour monter l'image / supprimer l'image
-
-//le bouton supprimer doit etre rouge
-
-//ne pas afficher le bouton a l'aide de l'intruction @if si l'image est dans la premiere categorie dans le cas du +
-// et le bouton - si l'image est dans la derniere categorie
